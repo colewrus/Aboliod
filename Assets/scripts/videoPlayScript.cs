5d9bfd65ffe9a3ad360f530f16f6dafce6ff3897
colewrus <wrampelc@gmail.com>
2017-03-22 16:52:17 -0400

    fixed the video playback. The streaming assets folder CANNOT have a space

--- Assets/scripts/videoPlayScript.cs
+++ Assets/scripts/videoPlayScript.cs
@@ -1,27 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class videoPlayScript : MonoBehaviour {
 
 
 	// Use this for initialization
 	void Start () {
+		
 		StartCoroutine (playRun ());
+
 	}
 
 
-	IEnumerator playRun(){
-		print ("hello");
+
+	IEnumerator playRun(){		
+
 		if (contant_Script.instance.spanish) {
 			Handheld.PlayFullScreenMovie ("video_SP.mp4", Color.black, FullScreenMovieControlMode.CancelOnInput);
 
 		} else if (!contant_Script.instance.spanish) {
 			Handheld.PlayFullScreenMovie ("video_EN.mp4", Color.black, FullScreenMovieControlMode.CancelOnInput);
+
 		}
 
+
 		yield return new WaitForEndOfFrame();
-		yield return new WaitForEndOfFrame();
+
 		SceneManager.LoadScene ("MenuScreen");
 
 	}
